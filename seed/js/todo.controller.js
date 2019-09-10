0a8a0b317cfc1c131b1c19344ca74ec55d2fc7d9
Muralimanohar <mano@Muralimanohars-MacBook-Pro.local>
2019-09-10 14:05:18 +0530

    Complete third lab excercise

--- seed/js/todo.controller.js
+++ seed/js/todo.controller.js
@@ -1,30 +1,55 @@
-function TodoController() {
-    this.list = [{
-        id: 0,
-        text: 'My first todo list',
-        completed: false,
+function TodoController($scope) {
+  this.total = 0;
+  this.completed = 0;
+  this.inComplete = 0;
+  this.newTodo = '';
+  this.list = [{
+      id: 0,
+      text: 'My first todo list',
+      completed: false,
     },
     {
-        id: 1,
-        text: 'My second todo list',
-        completed: true,
+      id: 1,
+      text: 'My second todo list',
+      completed: true,
     },
     {
-        id: 2,
-        text: 'My third todo list',
-        completed: false,
+      id: 2,
+      text: 'My third todo list',
+      completed: false,
     },
     {
-        id: 3,
-        text: 'My fourth todo list',
-        completed: false,
-    }];
+      id: 3,
+      text: 'My fourth todo list',
+      completed: false,
+    }
+  ];
 
+  this.add = function () {
+    this.list.unshift({
+      id: this.total,
+      text: this.newTodo,
+      completed: false,
+    });
+    this.newTodo = '';
+    this.updateCount();
+  };
+  this.remove = function (index) {
+    this.list.splice(index, 1);
+    this.updateCount();
+  };
+  this.updateCount = function () {
     this.total = this.list.length;
-    this.completed = this.list.filter(function(eachTodo) { return eachTodo.completed; }).length;
-    this.inComplete = this.total -  this.completed;
+    this.completed = this.list.filter(function (eachTodo) {
+      return eachTodo.completed;
+    }).length;
+    this.inComplete = this.total - this.completed;
+  };
+
+  this.updateCount();
 }
 
+// TodoController.$inject = ['$scope'];
 angular
-    .module('app')
-    .controller('TodoController', TodoController);
+  .module('app')
+  .controller('TodoController', TodoController);
