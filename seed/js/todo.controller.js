0e60ad4938ca0df92618882cf49eb5b0e50073cd
Muralimanohar <mano@Muralimanohars-MacBook-Pro.local>
2019-09-10 11:35:26 +0530

    Compete second lab exercise

--- seed/js/todo.controller.js
+++ seed/js/todo.controller.js
@@ -1,20 +1,28 @@
 function TodoController() {
     this.list = [{
+        id: 0,
         text: 'My first todo list',
         completed: false,
     },
     {
+        id: 1,
         text: 'My second todo list',
-        completed: false,
+        completed: true,
     },
     {
+        id: 2,
         text: 'My third todo list',
         completed: false,
     },
     {
+        id: 3,
         text: 'My fourth todo list',
         completed: false,
     }];
+
+    this.total = this.list.length;
+    this.completed = this.list.filter(function(eachTodo) { return eachTodo.completed; }).length;
+    this.inComplete = this.total -  this.completed;
 }
 
 angular
