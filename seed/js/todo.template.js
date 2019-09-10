bd6e851874c74e9d9513d02065e514e981e1453c
Muralimanohar <mano@Muralimanohars-MacBook-Pro.local>
2019-09-10 18:07:04 +0530

    Complete sixth exercise part two

---
+++ seed/js/todo.template.js
@@ -0,0 +1,78 @@
+return `
+< div ng - controller = "TodoController as todo" >
+  <
+  div >
+  <
+  h2 > Status < /h2> <
+  div >
+  Total: < span > {
+    {
+      todo.total
+    }
+  } < /span> <
+  /div> <
+  div >
+  completed: < span > {
+    {
+      todo.completed
+    }
+  } < /span> <
+  /div> <
+  div >
+  incomplete: < span > {
+    {
+      todo.inComplete
+    }
+  } < /span> <
+  /div> <
+  /div> <
+  div >
+  <
+  form ng - submit = "todo.add();" >
+  <
+  input type = "text"
+ng - model = "todo.newTodo" / >
+  <
+  /form> <
+  /div> <
+  div class = "todo" >
+  <
+  ul class = "todo__list" >
+  <
+  li ng - repeat = "eachTodo in todo.list" >
+  <
+  input type = "checkbox"
+id = "todo-{{eachTodo.id}}"
+ng - model = "eachTodo.completed"
+ng - change = "todo.toggle(eachTodo, $index);" / >
+  <
+  label class = "toggle"
+for = "todo-{{eachTodo.id}}" > < /label> <
+p ng - dblclick = "oldTodo = eachTodo.title; editMode = true;"
+ng - hide = "editMode" > {
+    {
+      eachTodo.title
+    }
+  } < /p> <
+  div ng - show = "editMode" >
+  <
+  input type = "text"
+ng - model = "eachTodo.title"
+ng - blur = "todo.update(eachTodo, $index, oldTodo); editMode = false;"
+placeholder = "Add New Todo"
+todo - autofocus = "editMode" / >
+  <
+  /div> <
+  a href = ""
+ng - click = "todo.remove(eachTodo, $index);" > X < /a> <
+  /li> <
+  /ul> <
+  /div> <
+  h1 ng - show = "todo.errorMessage"
+style = "color: red;" > {
+    {
+      todo.errorMessage
+    }
+  } < /h1> <
+  /div>
+`;
